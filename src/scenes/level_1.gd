5d425bc025bacb73185456307446c4e83ae67ba3
Brnobossicek <tobik.brnak@icloud.com>
2026-04-14 23:51:51 +0100

    Added function to pass camera from net_player

--- src/scenes/level_1.gd
+++ src/scenes/level_1.gd
@@ -3,12 +3,23 @@ extends Node3D
 func _ready() -> void:
 	print("LEVEL1 rdy")
 	Net.configure(self, $Players, $Items)
+	Net.player_spawned.connect(_on_player_spawned)
 	print("CONFIGURED")
 	Net.start_pending_connection()
-	print("START_PENDING_COLLECTION_CALLED")
+	print("START_PENDING_CONNECTION_CALLED")
 
 
+func _on_player_spawned(peer_id: int, player: Node) -> void:
+	if str(multiplayer.get_unique_id()) != player.name:
+		return
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+	var terrain = $Terrain3D
+
+	if player.has_node("Pivot/Camera3D"):
+		var camera = player.get_node("Pivot/Camera3D") as Camera3D
+		terrain.set_camera(camera)
+		print("Terrain camera set for local player: ", peer_id)
+
+
+func _process(_delta: float) -> void:
 	pass
