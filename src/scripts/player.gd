2cfda5a439c91c52b137b075ef82ec7f0bcd9081
michal <michal.dev@nightlion.cz>
2026-04-19 12:21:30 +0200

    Add script for jump animation.

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -4,16 +4,22 @@ extends CharacterBody3D
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+#animation path
 @onready var anim = $magician/AnimationPlayer
+
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		#animation jump
+		if anim.current_animation != "jump":
+			anim.play("jump")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -22,15 +28,14 @@ func _physics_process(delta: float) -> void:
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
-		
-		if anim.current_animation != "walk":
+		#animation walk
+		if anim.current_animation != "walk" and anim.current_animation != "jump":
 			anim.play("walk")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-		
-		
-		if anim.current_animation != "idle":
+		#animation idle
+		if anim.current_animation != "idle" and anim.current_animation != "jump":
 			anim.play("idle")
 
 	move_and_slide()
